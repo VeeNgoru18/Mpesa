da6e5ebe09b56c07c586128b1ecf39153e85be13
Virginia Ngoru <43510563+VeeNgoru18@users.noreply.github.com>
2020-09-06 23:57:54 +0300

    added options :anchor:

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package mpesa
+
+fun main() {
+    println("1. Send Money")
+    println("2. Withdraw Cash")
+    println("3. Buy Airtime")
+    println("4. Loans and Saving")
+    println("5. Lipa na M-PESA")
+    println("6. My Account")
 }
